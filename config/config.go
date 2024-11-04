c79db123ede2d9fe8cbf03b438f5af27363c0f70
N Rizki Amalia <rizkiamel9@gmail.com>
2024-11-04 19:08:07 +0700

    fix cors

--- config/config.go
+++ config/config.go
@@ -3,27 +3,23 @@ package config
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
+// MongoURI and DBName are global variables that hold the database URI and database name.
 var MongoURI string
 var DBName string
 
+// LoadEnv retrieves environment variables for MongoDB configuration.
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	// Get MongoDB connection URI from the environment
 	MongoURI = os.Getenv("MONGO_URI")
 	if MongoURI == "" {
-		log.Fatal("MONGO_URI not set in .env file")
+		log.Fatal("MONGO_URI environment variable is not set")
 	}
 
+	// Get the database name from the environment
 	DBName = os.Getenv("DB_NAME")
 	if DBName == "" {
-		log.Fatal("DB_NAME not set in .env file")
+		log.Fatal("DB_NAME environment variable is not set")
 	}
 }
